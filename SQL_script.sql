8ce854f80e266b82163d582263e632f63f56b9d5
MichaelaSty <126605025+MichaelaSty@users.noreply.github.com>
2023-04-12 16:41:49 +0200

    Update SQL_script.sql

--- SQL_script.sql
+++ SQL_script.sql
@@ -323,4 +323,27 @@ WHERE
 		- increase the prices or slows down the price reduction trend 
 		- increase the wages
 */
-	
+
+-- Creating secondary table
+CREATE OR REPLACE TABLE t_michaela_styskalova_project_SQL_secondary_final AS (
+SELECT
+	c.country,
+	c.continent,
+	c.population,
+	c.region_in_world,
+	e.`year`,
+	round(e.GDP,0) AS 'GDP',
+	e.gini
+FROM countries c
+LEFT JOIN economies e
+ON c.country = e.country
+WHERE continent = 'Europe' AND `year` BETWEEN 2000 AND 2021
+GROUP BY 
+	c.country,
+	c.continent,
+	c.population,
+	c.region_in_world,
+	e.`year`,
+	e.GDP,
+	e.gini
+);
